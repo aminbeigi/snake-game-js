f40cd97193347e454ba719ad84f78aee73f9c99e
aminbeigi <me@aminbeigi.com>
2022-01-29 14:26:00 +1100

    Add a snake to board

--- src/board.js
+++ src/board.js
@@ -11,38 +11,58 @@ import { Square } from "./square.js";
 "use strict";
 
 const MEDIUM = "medium"; // 9x9
-
+const EMPTY = 0;
+const SNAKE = 1;
+const APPLE = 2;
 export class Board {
-    #EMPTY = 1;
+    #board;
+    #boardSize;
 
     constructor(boardSize) {
-        this.boardSize = boardSize;
-        this.board = this.initBoard();
+        this.#boardSize = boardSize;
+        this.#board = this.#initBoard();
+        this.#initSnake();
+
     }
 
     static get medium() {
         return MEDIUM;
     }
 
-    initBoard() {
-        console.log("Initialising board.");
+    static get empty() {
+        return EMPTY;
+    }
 
+    static get snake() {
+        return SNAKE;
+    }
 
+    static get empty() {
+        return EMPTY;
+        
+    }
 
+    #initBoard() {
         let containerElement = document.getElementById("container");
-        let board = [];
+        let board = []
         for (let row = 0; row < 9; ++row) {
+            board.push(Array(9).fill(null));
             let rowElement = document.createElement("div");
             containerElement.append(rowElement);
             for (let col = 0; col < 9; ++col) {
-                let square = new Square(0, null);
                 let squareElement = document.createElement("div");
-                squareElement.className = "square";
-                squareElement.innerText = 3;
+                squareElement.className = "square-empty";
                 rowElement.append(squareElement);
+                board[row][col] = new Square(3, squareElement);
             }
         }
         console.log(board);
         return board;
+    }
+
+    #initSnake() {
+        this.#board[4][1].type = Board.snake;
+        this.#board[4][2].type = Board.snake;
+        this.#board[4][3].type = Board.snake;
     }
 }
--- src/square.js
+++ src/square.js
@@ -1,6 +1,41 @@
+import { Board } from "./board.js";
+
 export class Square {
     constructor(type, element) {
-        this.type = type;
+        this._type = type;
         this.element = element;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    set type(type) {
+        if (!Square.#isValidType) {
+            throw new Error(`Type must be either 1, 2 or 3. ${type} is not a valid type.`)
+        }
+
+        if (this._type === type) {
+            throw new Error(`Can not set type to same type.`)
+        }
+
+        switch (type) {
+            case Board.empty:
+                this.element.className = "square-empty";
+                break;
+            case Board.snake:
+                this.element.className = "square-snake";
+                break;
+            case Board.apple:
+                this.element.className = "square-apple";
+                break;
+            default:
+                throw Error(`${type} is not a valid type.`);
+        }
+        this._type = type;
+    }
+
+    static #isValidType(type) {
+        return type === Board.empty || type === Board.snake || type === Board.apple;
     }
 }
