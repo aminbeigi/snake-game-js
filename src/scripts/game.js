d7e66f740399c8ab2ea94591b7a0b84d036d9b86
aminbeigi <me@aminbeigi.com>
2022-01-30 15:52:13 +1100

    Finished functionality of menu

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,55 +2,76 @@
 
 import { Board } from "./board.js";
 
-const showGameScreen = () => {
-    document.getElementById("game-screen").style.display = "block";
-    document.getElementById("game-menu").style.display = "none";
-}
-
-const showMenuScreen = () => {
-    document.getElementById("game-screen").style.display = "none";
-    document.getElementById("game-menu").style.display = "block";
-}
-
-
-showMenuScreen();
-const gameDataFormElement = document.getElementById("gameDataForm")
-
-gameDataFormElement.addEventListener('click', () => {
-    const boardSizeFormValue = document.getElementById("board-size").value;
-    const gameSpeedFormValue = document.getElementById("game-speed").value;
-    let boardSize;
-    let gameSpeed;
-
-    switch (boardSizeFormValue) {
-        case '0':
-            boardSize = Board.boardSizeSmall;
-            break;
-        case '1':
-            boardSize = Board.boardSizeMedium;
-            break;
-        case '2':
-            boardSize = Board.boardSizeLarge;
-            break;
-        default:
-            throw Error(`Do not recognise ${boardSize} value from form.`);
+class Game {
+    constructor() {
+        this._showMenuScreen();
+        this._initStartGameButton();
+        this._initExitGameButton();
+    }
+    _showGameScreen() {
+        document.getElementById("game-screen").style.display = "block";
+        document.getElementById("game-menu").style.display = "none";
     }
 
-    switch (gameSpeedFormValue) {
-        case '0':
-            gameSpeed = Board.gameSpeedSlow;
-            break;
-        case '1':
-            gameSpeed = Board.gameSpeedMedium;
-            break;
-        case '2':
-            gameSpeed = Board.gameSpeedFast;
-            break;
-        default:
-            throw Error(`Do not recognise ${gameSpeedFormValue} value from form.`);
+    _showMenuScreen() {
+        document.getElementById("game-screen").style.display = "none";
+        document.getElementById("game-menu").style.display = "block";
     }
 
-    const board = new Board(boardSize, gameSpeed);
-    showGameScreen();
-})
+    _initStartGameButton() {
+        document.getElementById("gameDataForm").addEventListener('click', () => this._startGameHandler());
+    }
 
+    _startGameHandler() {
+        const boardSizeFormValue = document.getElementById("board-size").value;
+        const gameSpeedFormValue = document.getElementById("game-speed").value;
+        this.board = new Board(Game._formValueToBoardSize(boardSizeFormValue),
+            Game._formValueToGameSpeed(gameSpeedFormValue));
+        this._showGameScreen();
+    }
+    
+    _initExitGameButton() {
+        document.querySelector(".exit-game").addEventListener('click', () => this._exitGameHandler());
+    }
+
+    _exitGameHandler() {
+        this._showMenuScreen();
+        const parent = document.getElementById("game-screen");
+        console.log(parent)
+        while (parent.firstChild) {
+            if (parent.lastChild.textContent === 'X') {
+                break;
+            }
+            parent.removeChild(parent.lastChild)
+        }
+
+    }
+
+    static _formValueToBoardSize(boardSizeFormValue) {
+        switch (boardSizeFormValue) {
+            case '0':
+                return Board.boardSizeSmall;
+            case '1':
+                return Board.boardSizeMedium;
+            case '2':
+                return Board.boardSizeLarge;
+            default:
+                throw Error(`Do not recognise ${boardSize} value from form.`);
+        }
+    }
+
+    static _formValueToGameSpeed(gameSpeedFormValue) {
+        switch (gameSpeedFormValue) {
+            case '0':
+                return Board.gameSpeedSlow;
+            case '1':
+                return Board.gameSpeedMedium;
+            case '2':
+                return Board.gameSpeedFast;
+            default:
+                throw Error(`Do not recognise ${gameSpeedFormValue} value from form.`);
+        }
+    }
+}
+
+const game = new Game();
